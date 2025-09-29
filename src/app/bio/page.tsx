c6f9e397baf51f95143d267e693a632c8c2fce66
Tineil_A <tine6573@gmail.com>
2025-09-29 08:57:26 -0400

    remade bio page

---
+++ src/app/bio/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const BioPage = () => {
+    return (
+        <div>
+            <h1>About Me</h1>
+            <p>Here's where you can learn more about me!</p>
+        </div>
+    );
+};
+
+export default BioPage;
