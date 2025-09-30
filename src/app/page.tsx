7d89b860896d7509964bca7e060142c7569c9489
Tineil_A <tine6573@gmail.com>
2025-09-30 19:55:50 -0400

    styled links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
+'use client';
+
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main>
-      <h1>Welcome</h1>
-      <p>I am the home page</p>
+      <h1>Hello</h1>
+      <p>Explore my work, dive into my projects, and get to know me better on the <Link href="/bio" className="about">about</Link> page.</p>
     </main>
   );
 }
