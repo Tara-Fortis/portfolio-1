441b4728d823fde2d87a0219c36ec5c1d5c0edfd
Tineil_A <tine6573@gmail.com>
2026-05-15 13:00:46 -0400

    Added Toggler to header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -7,7 +7,6 @@ import * as React from 'react';
 import { Container } from "react-bootstrap";
 import Nav from "react-bootstrap/Nav";
 import { Navbar } from "react-bootstrap";
-import { NavDropdown } from "react-bootstrap";
 
 export default function Header() {
     // get the current pathname
@@ -23,30 +22,31 @@ export default function Header() {
         <Navbar expand="lg" className="navbar-dark">
             <Container>
                 <Navbar.Brand href="/">
-                        <Image
-                            src="/tinedev.svg"
-                            width={300}
-                            height={47}
-                            alt="Brand logo, a stylized outline representing a brain with text that spells 'tinedev'"
-                            priority // load image first
-                        />
+                    <Image
+                        src="/tinedev.svg"
+                        width={300}
+                        height={47}
+                        alt="Brand logo, a stylized outline representing a brain with text that spells 'tinedev'"
+                        priority // load image first
+                    />
                 </Navbar.Brand>
                 <Navbar.Toggle />
-                <Navbar.Collapse id="navBarToggler"/>
-                <Nav className="collapse navbar-collapse" >
-                    <ul className="navbar-nav me-auto">
-                        {navLinks.map(({ href, label }) => (
-                            <li key={href} className="nav-item">
-                                {/* If current route matches Link, apply 'active' class */}
-                                <Link
-                                    href={href}
-                                    className={`nav-link ${pathname === href ? "active" : ""}`}
-                                >{label}
-                                </Link>
-                            </li>
-                        ))}
-                    </ul>
-                </Nav>
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto" >
+                        <Nav className="navbar-nav me-auto">
+                            {navLinks.map(({ href, label }) => (
+                                <li key={href} className="nav-item">
+                                    {/* If current route matches Link, apply 'active' class */}
+                                    <Link
+                                        href={href}
+                                        className={`nav-link ${pathname === href ? "active" : ""}`}
+                                    >{label}
+                                    </Link>
+                                </li>
+                            ))}
+                        </Nav>
+                    </Nav>
+                </Navbar.Collapse>
             </Container>
         </Navbar>
     );
